c3c930777d05ec30bf3fc00596cf2ed122fe830c
Heidi Kumar <heidi.kristine.hansen@gmail.com>
2015-07-15 15:33:09 -0700

    Added _.each and _filter

--- src/underbar.js
+++ src/underbar.js
@@ -55,6 +55,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+      if(Array.isArray(collection)){
+        for (var i=0; i<collection.length; i++){
+          iterator(collection[i], i, collection);
+        }
+      } else {
+       for (var key in collection){
+          iterator(collection[key], key, collection);
+       }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -76,6 +85,13 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+      var newArr = [];
+      _.each(collection, function(element){
+        if(test(element)){
+          newArr.push(element);
+        }
+      })
+      return newArr;
   };
 
   // Return all elements of an array that don't pass a truth test.
